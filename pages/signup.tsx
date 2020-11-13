e2c80336af14a9a2a8f974a8ff1f995d90652c22
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-13 10:17:39 -0800

    Added yup validation

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,7 @@
 import { gql, useMutation } from '@apollo/client';
 import { useFormik } from 'formik';
+import { useState } from 'react';
+import { object, string } from 'yup';
 import { User } from '../components/entities';
 
 const REGISTER_USER = gql`
@@ -13,13 +15,40 @@ const REGISTER_USER = gql`
   }
 `;
 
+function PasswordValidationCard({ error }: { error: string }) {
+  return (
+    <div>
+      <p>{error}</p>
+    </div>
+  );
+}
+
 export default function SignUp() {
+  const [isFocused, setFocused] = useState(false);
+
+  const SignupSchema = object().shape({
+    email: string().email('Invalid email').required('Required'),
+    password: string()
+      .required('Required')
+      .matches(
+        /^(?=.*\d{2})(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/,
+        'Must have atleast 6 characters and 2 numbers'
+      ),
+    confirmPassword: string()
+      .required('Required')
+      .test('password-match', 'Passwords do not match', function (value) {
+        const { password } = this.parent;
+        return password === value;
+      }),
+  });
+
   const [registerUser] = useMutation<User, User>(REGISTER_USER);
 
-  const formik = useFormik({
+  const { handleSubmit, handleChange, handleBlur, values, errors } = useFormik({
     initialValues: {
       email: '',
       password: '',
+      confirmPassword: '',
     },
     onSubmit: values => {
       registerUser({
@@ -29,33 +58,62 @@ export default function SignUp() {
         },
       });
     },
+    validationSchema: SignupSchema,
+    validateOnChange: false,
+    validateOnBlur: true,
   });
 
   return (
     <div id='container'>
-      <div id='card'>
-        <div id='left-column'>
-          <form onSubmit={formik.handleSubmit}>
-            <h1>Sign Up</h1>
-            <label htmlFor='email'>Email</label>
-            <input
-              name='email'
-              type='text'
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-            <label htmlFor='password'>Password</label>
-            <input
-              name='password'
-              type='password'
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-            <button type='submit'>Sign Up</button>
-          </form>
-        </div>
-        <div id='right-column' />
+      <div id='left-column'>
+        <form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
+          <label htmlFor='email'>
+            Email
+            <span>{errors.email && ' *' + errors.email}</span>
+          </label>
+          <input
+            name='email'
+            type='text'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          <label htmlFor='password'>
+            Password
+            <span>{errors.password && ' *' + errors.password}</span>
+            {isFocused && <PasswordValidationCard error={errors.password} />}
+          </label>
+          <input
+            name='password'
+            type='password'
+            onChange={handleChange}
+            value={values.password}
+            onFocus={() => {
+              setFocused(true);
+            }}
+            onBlur={e => {
+              handleBlur(e);
+              setFocused(false);
+            }}
+          />
+          <label htmlFor='confirmPassword'>
+            Confirm Password
+            <span>
+              {errors.confirmPassword && ' *' + errors.confirmPassword}
+            </span>
+          </label>
+          <input
+            name='confirmPassword'
+            type='password'
+            onChange={handleChange}
+            value={values.confirmPassword}
+          />
+          <button type='submit'>Sign Up</button>
+        </form>
+        <div id='background' />
       </div>
+      <div id='right-column' />
 
       <style jsx>{`
         #container {
@@ -64,54 +122,58 @@ export default function SignUp() {
           align-items: center;
 
           height: 100vh;
-          background-image: linear-gradient(35deg, #c2f3fc, #bfa4f5);
+          background-image: linear-gradient(35deg);
         }
 
-        #card {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-
-          width: 80%;
-          height: 85%;
-
+        #card,
+        #left-column,
+        #right-column {
           background-color: white;
-
-          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
-            0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
 
         #left-column,
         #right-column {
-          background-color: white;
           float: left;
           width: 50%;
+          height: 100%;
+        }
+
+        #password-card {
+          position: absolute;
+        }
+
+        #left-column {
+          display: flex;
+          align-items: center;
         }
 
         #right-column {
           background-image: url('/cards.svg');
-          object-fit: scale-down;
-          height: 100%;
-        }
-
-        body {
         }
 
         form {
           display: inline-flex;
           flex-direction: column;
           margin: 0px 25%;
+
           width: 200px;
         }
 
         label,
         input,
         h1,
-        p {
+        p,
+        span {
           font-family: 'Lato', sans-serif;
+          color: black;
+        }
+
+        span {
+          color: red;
         }
 
         input {
+          background-color: transparent;
           font-size: 14px;
           border: none;
           border-bottom: 1.5px solid black;
@@ -125,6 +187,33 @@ export default function SignUp() {
         button {
           width: 100px;
           margin: 10px auto;
+        }
+
+        @media screen and (max-width: 850px) {
+          #left-column {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            z-index: 0;
+          }
+
+          #right-column {
+            display: none;
+          }
+
+          #background {
+            position: absolute;
+            background-image: url('/cards.svg');
+            filter: blur(8px);
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+          }
+
+          input {
+            border: 1.5px solid transparent;
+            border-bottom: 1.5px solid black;
+          }
         }
       `}</style>
     </div>
