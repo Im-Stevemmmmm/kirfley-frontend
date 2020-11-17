96efb183ffa323685be22a71e1765f17c4fce196
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-16 20:35:16 -0800

    Completed index unit test

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Homepage from '../../pages';
 
-test('homepage is rendered', () => {
+test('greeting is displayed', () => {
   render(<Homepage />);
 
-  const title = screen.getByText('Fern');
-  expect(title).toBeInTheDocument();
+  expect(screen.getByText('Fern')).toBeInTheDocument;
+  expect(
+    screen.getByText('A free platform where you can pretty much say anything.')
+  ).toBeInTheDocument;
+});
+
+test('sign up and log in buttons are rendered', () => {
+  render(<Homepage />);
+
+  expect(screen.getByRole('button', { name: 'Sign Up' })).toBeInTheDocument;
+  expect(screen.getByRole('button', { name: 'Log In' })).toBeInTheDocument;
 });
