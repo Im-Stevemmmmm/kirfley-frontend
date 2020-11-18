c415dd706f1f4ee2f76eaec5711375fa34ff4a4c
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-18 15:03:15 -0800

    Added util folder ignore path

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   setupFiles: ['<rootDir>/setup-tests.ts'],
+  modulePathIgnorePatterns: ['__tests__/__utils__/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
   },
