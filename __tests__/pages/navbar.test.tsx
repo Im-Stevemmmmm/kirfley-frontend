06f403f8db99524b6cd126bc42965cb0f872e7c6
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-19 00:20:21 -0800

    Added navbar and unit tests

---
+++ __tests__/pages/navbar.test.tsx
@@ -0,0 +1,27 @@
+import { act, render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import Navbar from '../../components/navbar';
+
+it('renders correctly', () => {
+  const container = document.createElement('div');
+
+  act(() => {
+    ReactDOM.render(<Navbar />, container);
+  });
+
+  expect(container).toMatchSnapshot();
+});
+
+describe('navbar', () => {
+  beforeEach(() => render(<Navbar />));
+
+  it('renders the sign up and log in buttons', () => {
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeInTheDocument;
+    expect(screen.getByRole('button', { name: 'Log In' })).toBeInTheDocument;
+  });
+
+  it('renders the fern logo', () => {
+    expect(screen.getByRole('heading').textContent).toBe('Fern');
+    expect(screen.getByAltText('Fern Picture')).toBeInTheDocument;
+  });
+});
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import buttonStyles from '../styles/button.module.css';
+import styles from '../styles/navbar.module.css';
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div id={styles.logo}>
+        <div id={styles.fernImg}>
+          <Image src='/fern.svg' width={60} height={60} alt='Fern Picture' />
+        </div>
+        <h2>Fern</h2>
+      </div>
+
+      <div id={styles.links}>
+        <Link href='/login'>
+          <button className={buttonStyles.standard}>Log In</button>
+        </Link>
+        <Link href='/signup'>
+          <button className={buttonStyles.inverted}>Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
