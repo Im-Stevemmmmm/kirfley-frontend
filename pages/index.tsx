a9a23f6916cb49c82fa05baff69bd9c20facece6
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-23 14:56:28 -0800

    Restyled index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,34 +7,6 @@ import buttonStyles from '../styles/button.module.css';
 import styles from '../styles/index.module.css';
 import { motion } from 'framer-motion';
 
-const OffsetLogo = ({ mirrored }: { mirrored?: boolean }) => {
-  interface StyleProperties {
-    classString: string;
-    styles: CSSProperties;
-  }
-
-  const distance = '-200px';
-
-  const styleMap: StyleProperties[] = [
-    {
-      classString: `${styles.fernImg}`,
-      styles: { left: distance },
-    },
-    {
-      classString: `${styles.fernImg} ${styles.mirrorHorizontal}`,
-      styles: { right: distance },
-    },
-  ];
-
-  const styleProperties = styleMap[mirrored ? 1 : 0];
-
-  return (
-    <div className={styleProperties.classString} style={styleProperties.styles}>
-      <Image src='/fern.svg' width={100} height={100} priority />
-    </div>
-  );
-};
-
 const TrendingCard = ({
   trend: heading,
   children,
@@ -51,46 +23,38 @@ const TrendingCard = ({
 };
 
 const Homepage = () => {
-  const [loginBoxIsVisible, setLoginBoxIsVisible] = useState(false);
+  const [loginBoxIsVisible, setLoginBoxVisibility] = useState(false);
 
   return (
     <div>
+      <Head>
+        <title>Fern</title>
+      </Head>
+
       <motion.div
         initial='hidden'
-        animate={loginBoxIsVisible ? 'visible' : 'hidden'}
+        animate={loginBoxIsVisible ? 'hidden' : 'visible'}
         variants={{
           hidden: {
             opacity: 0,
+            transitionEnd: {
+              display: 'none',
+            },
           },
           visible: {
             opacity: 1,
+            display: 'block',
           },
         }}
       >
         <IndexNavbar />
       </motion.div>
 
-      <div id={styles.banner}>
-        <Head>
-          <title>Fern</title>
-        </Head>
-
-        <h1>Fern</h1>
-
-        <div id={styles.horizontalBarContainer}>
-          <OffsetLogo mirrored />
-          <div id={styles.horizontalBar} />
-          <OffsetLogo />
-        </div>
-
-        <h2 className={styles.wrappedText}>The Open Social Media Platform</h2>
-      </div>
-
       <div id={styles.sectionOne}>
         <div id={styles.rightColumn}>
           <VisibilitySensor
             onChange={visible => {
-              setLoginBoxIsVisible(!visible);
+              setLoginBoxVisibility(visible);
             }}
             partialVisibility
           >
@@ -102,61 +66,14 @@ const Homepage = () => {
 
         <div id={styles.leftColumnContainer}>
           <div id={styles.leftColumn}>
-            <h1>The Open Social Media Platform</h1>
-
-            <p>
-              Enjoy a free application where you can communicate with others
-              without fear of your ideas being censored. Or some other random
-              description like this. This is a placeholder!
-            </p>
-
-            <button className={buttonStyles.inline}>
-              Expirement{' '}
+            <h1 id={styles.title}>
+              Fern
               <span>
-                <Image src='/arrow.svg' width={10} height={10} priority />
+                <Image src='/fern.svg' width='125px' height='75px' priority />
               </span>
-            </button>
-          </div>
-        </div>
-        <div id={styles.leftColumnContainer}>
-          <div id={styles.leftColumn}>
-            <h1>The Open Social Media Platform</h1>
+            </h1>
 
-            <p>
-              Enjoy a free application where you can communicate with others
-              without fear of your ideas being censored. Or some other random
-              description like this. This is a placeholder!
-            </p>
-
-            <button className={buttonStyles.inline}>
-              Expirement{' '}
-              <span>
-                <Image src='/arrow.svg' width={10} height={10} priority />
-              </span>
-            </button>
-          </div>
-        </div>
-        <div id={styles.leftColumnContainer}>
-          <div id={styles.leftColumn}>
-            <h1>The Open Social Media Platform</h1>
-
-            <p>
-              Enjoy a free application where you can communicate with others
-              without fear of your ideas being censored. Or some other random
-              description like this. This is a placeholder!
-            </p>
-
-            <button className={buttonStyles.inline}>
-              Expirement{' '}
-              <span>
-                <Image src='/arrow.svg' width={10} height={10} priority />
-              </span>
-            </button>
-          </div>
-        </div>
-        <div id={styles.leftColumnContainer}>
-          <div id={styles.leftColumn}>
-            <h1>The Open Social Media Platform</h1>
+            <h1>The True Social Media Platform</h1>
 
             <p>
               Enjoy a free application where you can communicate with others
