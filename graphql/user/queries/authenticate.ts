c081975960f8fb7b0a75d8dbe4ecab6ab10a224f
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-23 19:43:35 -0800

    Moved gql operations to .graphql files

--- graphql/user/queries/authenticate.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const AUTHENTICATE_USER = gql`
-  query AuthenticateUser($email: String!, $password: String!) {
-    login(options: { email: $email, password: $password }) {
-      successful
-    }
-  }
-`;
--- graphql/user/queries/check-field-availability.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const CHECK_FIELD_AVAILABILITY = gql`
-  query CheckFieldAvailabilty($field: String!, $value: String!) {
-    checkFieldAvailability(field: $field, value: $value) {
-      successful
-    }
-  }
-`;
