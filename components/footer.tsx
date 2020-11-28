a4d6a009905075996098f0c9e16b18fc702eefdc
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-27 18:26:29 -0800

    Converted to normal function

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,35 +1,31 @@
 import Link from 'next/link';
-import styles from '../styles/footer.module.css';
+import styles from '../styles/footer.module.scss';
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div id={styles.container}>
-      <footer id={styles.footer}>
-        <ul className={styles.column}>
-          <li>About</li>
-          <li title='About the company behind Fern'>
-            <Link href='/support'>Company</Link>
-          </li>
-        </ul>
-        <ul className={styles.column}>
-          <li>Support</li>
-          <li title='Get help with any problems'>
-            <Link href='/'>Contact Us</Link>
-          </li>
-        </ul>
-        <ul className={styles.column}>
-          <li>Legal</li>
-          <li title="Fern's privacy policy">
-            <Link href='/'>Privacy Policy</Link>
-          </li>
-          <li title='How we use cookies'>
-            <Link href='/'>Cookies</Link>
-          </li>
-          <li id={styles.copyright}>&copy; Fern {new Date().getFullYear()}</li>
-        </ul>
-      </footer>
-    </div>
+    <footer id={styles['footer']}>
+      <ul className={styles.column}>
+        <li>About</li>
+        <li title='About the company behind Fern'>
+          <Link href='/support'>Company</Link>
+        </li>
+      </ul>
+      <ul className={styles.column}>
+        <li>Support</li>
+        <li title='Get help with any problems'>
+          <Link href='/'>Contact Us</Link>
+        </li>
+      </ul>
+      <ul className={styles.column}>
+        <li>Legal</li>
+        <li title="Fern's privacy policy">
+          <Link href='/'>Privacy Policy</Link>
+        </li>
+        <li title='How we use cookies'>
+          <Link href='/'>Cookie Policy</Link>
+        </li>
+        <li id={styles.copyright}>&copy; Fern {new Date().getFullYear()}</li>
+      </ul>
+    </footer>
   );
-};
-
-export default Footer;
+}
