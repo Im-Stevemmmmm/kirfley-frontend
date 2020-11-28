a5da4247e02f23bafe1f4b1345233ebc83b05c84
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-27 18:21:20 -0800

    Added label component for auth forms

---
+++ components/forms/auth-form-label.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import styles from '../../styles/auth-form.module.scss';
+
+export default function AuthFormLabel({
+  name,
+  errors,
+  children,
+}: {
+  name: string;
+  errors: any;
+  children: React.ReactNode;
+}) {
+  return (
+    <label
+      className={classNames(styles['form__label'], {
+        [styles['form__label--error']]: errors[name],
+      })}
+      htmlFor={name}
+    >
+      {children}
+    </label>
+  );
+}
