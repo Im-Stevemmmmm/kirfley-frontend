108fdc9701f6f1179667840988b7891baf5a2bbb
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-27 18:09:16 -0800

    Changed setup-tests.js directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/setup-tests.js'],
+  setupFiles: ['<rootDir>/__tests__/__utils__/setup-tests.js'],
   modulePathIgnorePatterns: ['__tests__/__utils__/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
