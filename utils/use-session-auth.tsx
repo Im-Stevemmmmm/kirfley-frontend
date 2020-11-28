a4ef3c8f25b0249ab9a598e20710b83ac14f592e
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-27 18:22:27 -0800

    Added session auth hook

---
+++ utils/use-session-auth.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useMeQuery } from '../generated/graphql-types';
+
+export default function useSessionAuth() {
+  const { data, loading } = useMeQuery();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !data?.me) {
+      router.replace('/');
+    }
+  }, [loading, data, router]);
+}
