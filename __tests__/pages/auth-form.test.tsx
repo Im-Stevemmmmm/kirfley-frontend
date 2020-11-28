eed5935857aeb491d27326a2b6b30a5eb0899953
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-11-27 18:26:57 -0800

    Added auth form snapshot test

---
+++ __tests__/pages/auth-form.test.tsx
@@ -0,0 +1,21 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { act } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import AuthForm from '../../components/forms/auth-form';
+
+describe('login form', () => {
+  it('renders properly', () => {
+    const container = document.createElement('div');
+
+    act(() => {
+      ReactDOM.render(
+        <MockedProvider addTypename={false}>
+          <AuthForm />
+        </MockedProvider>,
+        container
+      );
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
