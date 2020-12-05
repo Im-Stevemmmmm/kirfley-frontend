73948a99af324bacf27a7fa283f479143e7d7f4f
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-05 02:19:27 -0800

    Added mock trending cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,91 @@
 import classNames from "classnames";
 import LoginForm from "components/auth-forms/login-form/login-form";
-import Footer from "components/footer/footer";
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "styles/pages/index.module.scss";
 import { websiteName } from "utils/constants/constants";
+
+interface TrendingCardProps {
+    title: string;
+    content: string;
+    url: string;
+}
+
+const dummyCards: TrendingCardProps[] = [
+    {
+        title: "#Geat",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "#YEET",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YEET",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YKEET",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "#G3ieeat",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "#YEE93T",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YEET39",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YKEET93",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "#Geat38207y5",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "#YEET10",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YEET1",
+        content: "HIII XD",
+        url: "/signup",
+    },
+    {
+        title: "YKEET2",
+        content: "HIII XD",
+        url: "/signup",
+    },
+];
+
+const TrendingCard = ({ title, content, url }: TrendingCardProps) => {
+    const router = useRouter();
+
+    return (
+        <div className={styles.trendingCard} onClick={() => router.push(url)}>
+            <h1>{title}</h1>
+
+            <p>{content}</p>
+        </div>
+    );
+};
 
 const Index = () => {
     return (
@@ -13,13 +94,13 @@ const Index = () => {
                 <title>{websiteName}</title>
             </Head>
 
-            <main>
+            <main id={styles.container}>
                 <div className={styles.banner}>
                     <div className={styles.banner__column}>
-                        <h1 className={styles.title}>
+                        <h1 className={styles.banner__title}>
                             {websiteName}
 
-                            <span className={styles.title__logo}>
+                            <span className={styles.banner__logo}>
                                 <Image
                                     src="/fern.svg"
                                     width="100%"
@@ -49,9 +130,28 @@ const Index = () => {
                         <LoginForm />
                     </div>
                 </div>
+
+                <div className={styles.trendingContainer}>
+                    <h1 className={styles.trendingContainer__title}>
+                        Connect with the world.
+                    </h1>
+
+                    <h2>Be the first to find out about the world.</h2>
+
+                    <div className={styles.trendingContainer__trendingCards}>
+                        {dummyCards.map(({ content, title, url }) => (
+                            <TrendingCard
+                                key={title}
+                                title={title}
+                                content={content}
+                                url={url}
+                            />
+                        ))}
+                    </div>
+                </div>
             </main>
 
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 };
