4989fc5ae690abc91b164b349fb3984edf5aaa7d
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 19:32:43 -0800

    Removed unused test utils

--- __tests__/__utils__/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { DocumentNode } from "@apollo/client";
-
-export interface ApolloMock {
-    request: { query: DocumentNode; variables: any };
-    result: { data: any };
-}
--- __tests__/__utils__/form.ts
+++
@@ -1,4 +0,0 @@
-import { fireEvent } from "@testing-library/react";
-
-export const setValue = (field: HTMLElement, value: string) =>
-    fireEvent.change(field, { target: { value } });
