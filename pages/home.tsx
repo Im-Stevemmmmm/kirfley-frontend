7897d095eb641c711102be19a8f56b9b94c97a37
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 19:37:37 -0800

    Changed to arrow function and added website name constant

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,13 +1,16 @@
-import styles from "../styles/home.module.scss";
-import useSessionAuth from "../utils/use-session-auth";
+import styles from "styles/pages/home.module.scss";
+import { websiteName } from "utils/constants/constants";
+import { useSessionAuth } from "utils/hooks/use-session-auth/use-session-auth";
 
-export default function Home() {
+const Home = () => {
     useSessionAuth();
 
     return (
         <div id={styles.container}>
-            <h1>Welcome to Fern!</h1>
+            <h1>Welcome to {websiteName}!</h1>
             <p>Recent posts...</p>
         </div>
     );
-}
+};
+
+export default Home;
