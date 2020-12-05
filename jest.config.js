09e6785c2113dccca3dd9d7a8fb322789db834d1
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 19:29:03 -0800

    Added baseUrl moduleNameMappings

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    setupFiles: ["<rootDir>/__tests__/__utils__/setup-tests.js"],
-    modulePathIgnorePatterns: ["__tests__/__utils__/"],
+    setupFilesAfterEnv: ["<rootDir>/setup-tests.js"],
     moduleNameMapper: {
-        "\\.(css|less|scss|sss|styl)$":
-            "<rootDir>/node_modules/jest-css-modules",
+        "\\.(css|scss|sass)$": "<rootDir>/node_modules/jest-css-modules",
+        "^components(.*)$": "<rootDir>/components$1",
+        "^generated(.*)$": "<rootDir>/generated$1",
+        "^utils(.*)$": "<rootDir>/utils$1",
+        "^styles(.*)$": "<rootDir>/styles$1",
+        "^pages(.*)$": "<rootDir>/pages$1",
     },
 };
