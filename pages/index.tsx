d5456d123641793795ec3d779cc5fe451eb1dccc
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 20:39:00 -0800

    Replaced float flexboxes with CSS grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,45 +7,40 @@ import { websiteName } from "utils/constants/constants";
 
 const Index = () => {
     return (
-        <div id={styles.backgroundImage}>
+        <div>
             <Head>
                 <title>{websiteName}</title>
             </Head>
 
             <main>
-                <div id={styles.banner}>
-                    <div className={styles.rightColumn}>
-                        <div className={styles.rightColumn__form}>
-                            <LoginForm />
-                        </div>
+                <div className={styles.banner}>
+                    <div className={styles.banner__child}>
+                        <h1 className={styles.title}>
+                            {websiteName}
+
+                            <span className={styles.title__logo}>
+                                <Image
+                                    src="/fern.svg"
+                                    width="100%"
+                                    height="75px"
+                                    alt="Fern picture"
+                                    priority
+                                />
+                            </span>
+                        </h1>
+
+                        <h2>The #1 Video Sharing Platform</h2>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Vivamus ultricies semper sagittis. Proin vitae
+                            est augue. Duis commodo, leo sed scelerisque
+                            porttitor, erat arcu lobortis velit.
+                        </p>
                     </div>
 
-                    <div className={styles.leftColumn}>
-                        <div className={styles.leftColumn__container}>
-                            <h1 className={styles.leftColumn__title}>
-                                {websiteName}
-
-                                <span className={styles.leftColumn__logo}>
-                                    <Image
-                                        src="/fern.svg"
-                                        width="100%"
-                                        height="75px"
-                                        alt="Fern picture"
-                                        priority
-                                    />
-                                </span>
-                            </h1>
-
-                            <h2>The #1 Video Sharing Platform</h2>
-
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Vivamus ultricies semper
-                                sagittis. Proin vitae est augue. Duis commodo,
-                                leo sed scelerisque porttitor, erat arcu
-                                lobortis velit.
-                            </p>
-                        </div>
+                    <div className={styles.banner__child}>
+                        <LoginForm />
                     </div>
                 </div>
             </main>
