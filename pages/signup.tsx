ada267cacf6578a35f7f5902c53a9a6e540170b5
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 19:36:42 -0800

    Added signup page

---
+++ pages/signup.tsx
@@ -0,0 +1,20 @@
+import SignupForm from "components/auth-forms/signup-form/signup-form";
+import Head from "next/head";
+import styles from "styles/pages/signup.module.scss";
+import { websiteName } from "utils/constants/constants";
+
+const Signup = () => {
+    return (
+        <div id={styles.container}>
+            <Head>
+                <title>Sign up | {websiteName}</title>
+            </Head>
+
+            <main id={styles.form}>
+                <SignupForm />
+            </main>
+        </div>
+    );
+};
+
+export default Signup;
