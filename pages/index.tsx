c764b1ffab40e4d24edf9234f08b2a6a886095d4
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-04 19:37:19 -0800

    Added website name constant and updated class names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,58 @@
+import LoginForm from "components/auth-forms/login-form/login-form";
+import Footer from "components/footer/footer";
 import Head from "next/head";
-import Footer from "../components/footer";
-import AuthForm from "../components/forms/auth-form";
-import styles from "../styles/index.module.scss";
+import Image from "next/image";
+import styles from "styles/pages/index.module.scss";
+import { websiteName } from "utils/constants/constants";
 
-export default function Homepage() {
+const Index = () => {
     return (
         <div id={styles.backgroundImage}>
             <Head>
-                <title>Fern</title>
+                <title>{websiteName}</title>
             </Head>
 
-            <div id={styles.mainContent}>
-                <div className={styles.rightColumn}>
-                    <div className={styles.rightColumn_form}>
-                        <AuthForm />
+            <main>
+                <div id={styles.banner}>
+                    <div className={styles.rightColumn}>
+                        <div className={styles.rightColumn__form}>
+                            <LoginForm />
+                        </div>
+                    </div>
+
+                    <div className={styles.leftColumn}>
+                        <div className={styles.leftColumn__container}>
+                            <h1 className={styles.leftColumn__title}>
+                                {websiteName}
+
+                                <span className={styles.leftColumn__logo}>
+                                    <Image
+                                        src="/fern.svg"
+                                        width="100%"
+                                        height="75px"
+                                        alt="Fern picture"
+                                        priority
+                                    />
+                                </span>
+                            </h1>
+
+                            <h2>The #1 Video Sharing Platform</h2>
+
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Vivamus ultricies semper
+                                sagittis. Proin vitae est augue. Duis commodo,
+                                leo sed scelerisque porttitor, erat arcu
+                                lobortis velit.
+                            </p>
+                        </div>
                     </div>
                 </div>
-
-                <div className={styles.leftColumn}>
-                    <div className={styles.leftColumn_container}>
-                        <h1 className={styles.leftColumn_title}>
-                            Fern
-                            <span className={styles.leftColumn_title_logo}>
-                                <img
-                                    src="/fern.svg"
-                                    width="27.5%"
-                                    height="75px"
-                                    alt="Fern picture"
-                                />
-                            </span>
-                        </h1>
-
-                        <h1>The True Social Media Platform</h1>
-
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Vivamus ultricies semper sagittis. Proin vitae
-                            est augue. Duis commodo, leo sed scelerisque
-                            porttitor, erat arcu lobortis velit.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            </main>
 
             <Footer />
         </div>
     );
-}
+};
+
+export default Index;
