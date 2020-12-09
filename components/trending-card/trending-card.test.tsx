1f01ddca08bb937c72dcdb3d0f4d0550eeedc43e
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-09 15:22:34 -0800

    Removed content from trending card

--- components/trending-card/trending-card.test.tsx
+++ components/trending-card/trending-card.test.tsx
@@ -2,19 +2,9 @@ import { fireEvent, render } from "@testing-library/react";
 import { TrendingCard } from "./trending-card";
 
 test("renders the proper title", () => {
-    const { getByText } = render(
-        <TrendingCard title="Steve" content="Some content" url="/signup" />
-    );
+    const { getByText } = render(<TrendingCard title="Steve" url="/signup" />);
 
     expect(getByText("Steve")).toBeInTheDocument;
-});
-
-test("renders the proper content", () => {
-    const { getByText } = render(
-        <TrendingCard title="Steve" content="Some content" url="/signup" />
-    );
-
-    expect(getByText("Some content")).toBeInTheDocument;
 });
 
 test("sends the user to the card's url on click", () => {
@@ -25,9 +15,7 @@ test("sends the user to the card's url on click", () => {
         push: routerPush,
     }));
 
-    const { getByText } = render(
-        <TrendingCard title="Steve" content="Some content" url="/signup" />
-    );
+    const { getByText } = render(<TrendingCard title="Steve" url="/signup" />);
 
     const trendingCard = getByText("Steve");
 
--- components/trending-card/trending-card.tsx
+++ components/trending-card/trending-card.tsx
@@ -3,18 +3,34 @@ import styles from "./trending-card.module.scss";
 
 interface TrendingCardProps {
     title: string;
-    content: string;
     url: string;
 }
 
-export const TrendingCard = ({ title, content, url }: TrendingCardProps) => {
+export const TrendingCard = ({ title, url }: TrendingCardProps) => {
     const router = useRouter();
 
     return (
         <div className={styles.trendingCard} onClick={() => router.push(url)}>
             <h1 className={styles.trendingCard__title}>{title}</h1>
-
-            <p>{content}</p>
         </div>
     );
 };
+
+export const mockTrends: TrendingCardProps[] = [
+    {
+        title: "#COVID-19",
+        url: "/signup",
+    },
+    {
+        title: "#GalacticFederation",
+        url: "/signup",
+    },
+    {
+        title: "#GithubDarkTheme",
+        url: "/signup",
+    },
+    {
+        title: "#Prisma",
+        url: "/signup",
+    },
+];
