4b70d73c81ac8cff85e564c66e4ef573530400a5
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-08 16:02:51 -0800

    Removed uneeded abstraction

--- utils/auth-format-error/auth-format-error.test.ts
+++ utils/auth-format-error/auth-format-error.test.ts
@@ -1,7 +1,5 @@
 import { formatError } from "./auth-format-error";
 
 test("properly formats an auth error", () => {
-    const error = "some error";
-
-    expect(formatError(error)).toBe(` *${error}`);
+    expect(formatError("some error")).toBe(` *some error`);
 });
