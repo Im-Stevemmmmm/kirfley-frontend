a1e6da04a54894ac0f81591eb737f8dfcce97e96
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2020-12-21 14:34:26 -0800

    Added css grid to home page & changed btn colors

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,14 +1,28 @@
+import { HomeContent } from "components/home/content/content";
+import { HomeNavbar } from "components/home/navbar/navbar";
+import { HomeSidebar } from "components/home/sidebar/sidebar";
+import Head from "next/head";
 import styles from "styles/pages/home.module.scss";
-import { websiteName } from "utils/constants/constants";
 import { useSessionAuth } from "utils/hooks/use-session-auth/use-session-auth";
 
 const Home = () => {
-    useSessionAuth();
+    // useSessionAuth();
 
     return (
-        <div id={styles.container}>
-            <h1>Welcome to {websiteName}!</h1>
-            <p>Recent posts...</p>
+        <div>
+            <Head>
+                <title>Home</title>
+            </Head>
+
+            <main>
+                <HomeNavbar />
+
+                <div className={styles.content}>
+                    <HomeSidebar />
+
+                    <HomeContent />
+                </div>
+            </main>
         </div>
     );
 };
