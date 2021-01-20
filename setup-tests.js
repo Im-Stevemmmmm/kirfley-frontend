92b5c14c4992e665de1fba853c4ff15ee37310ab
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-19 19:47:00 -0800

    Formatted files

--- setup-tests.js
+++ setup-tests.js
@@ -6,11 +6,11 @@ jest.mock("next/link", () => children());
 jest.mock("next/head", () => children());
 
 process.env = {
-    ...process.env,
-    __NEXT_IMAGE_OPTS: {
-        deviceSizes: [320, 420, 768, 1024, 1200],
-        imageSizes: [],
-        path: "/_next/image",
-        loader: "default",
-    },
+  ...process.env,
+  __NEXT_IMAGE_OPTS: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+    path: "/_next/image",
+    loader: "default",
+  },
 };
